6cd543a846fff89612cdd5ccab2795274cfa99d9
Prakriti <prakriti.kshyp@gmail.com>
2025-04-11 23:45:30 +0530

    Time: 20 ms (45.05%), Space: 38 MB (38.88%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    void dfs(int row, int col, vector<vector<int>>& grid,
+             vector<vector<int>>& vis, vector<pair<int, int>>& vec, int row0,
+             int col0) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vis[row][col] = 1;
+        vec.push_back({row - row0, col - col0});
+
+        vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for (auto dir : directions) {
+            int nrow = row + dir.first;
+            int ncol = col + dir.second;
+
+            if (nrow >= 0 and ncol >= 0 and nrow < n and ncol < m and
+                !vis[nrow][ncol] and grid[nrow][ncol] == 1) {
+                dfs(nrow, ncol, grid, vis, vec, row0, col0);
+            }
+        }
+    }
+
+public:
+    int numDistinctIslands(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        set<vector<pair<int, int>>> st;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] and grid[i][j] == 1) {
+                    vector<pair<int, int>> vec;
+
+                    dfs(i, j, grid, vis, vec, i, j);
+                    st.insert(vec);
+                }
+            }
+        }
+        return st.size();
+    }
+};
