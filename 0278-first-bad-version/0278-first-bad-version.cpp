62284f537ecc7942e33e3753356056cfb367dd36
Prakriti <prakriti.kshyp@gmail.com>
2025-04-17 17:44:03 +0530

    Time: 2 ms (56.37%), Space: 7.9 MB (38.13%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,11 +7,11 @@ public:
         int start = 1;
         int end = n;
 
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
 
             if (isBadVersion(mid)) {
-                end = mid - 1;
+                end = mid;
             } else {
                 start = mid + 1;
             }
