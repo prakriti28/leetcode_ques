3e845ee543281d7c50bc633ae4a532c9c7f2fed2
Prakriti <prakriti.kshyp@gmail.com>
2025-05-13 17:52:34 +0530

    Time: 898 ms (17.24%), Space: 467.1 MB (24.63%) - LeetHub

---
+++ 3335-total-characters-in-string-after-transformations-i/3335-total-characters-in-string-after-transformations-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        vector<long long> freq(26, 0);
+        const int MOD = 1e9 + 7;
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+        for (int j = 0; j < t; j++) {
+            vector<long long> tmp(26, 0);
+            for (int i = 0; i < 26; i++) {
+                if (char(i + 'a') != 'z') {
+                    tmp[i + 1] = (tmp[i + 1] + freq[i]) % MOD;
+                } else {
+                    tmp[0] = (tmp[0] + freq[i]) % MOD;
+                    tmp[1] = (tmp[1] + freq[i]) % MOD;
+                }
+            }
+            freq = tmp;
+        }
+        long long len =0;
+        for (long long c : freq) {
+            cout << c << endl;
+            len+=c;
+        }
+
+        return len%MOD;
+    }
+};
