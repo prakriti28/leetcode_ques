459e066168560f5030ce330a0cd7d027638bcdc8
Prakriti <prakriti.kshyp@gmail.com>
2025-06-22 12:06:35 +0530

    Time: 0 ms (100%), Space: 24.5 MB (22.97%) - LeetHub

--- 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.cpp
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.cpp
@@ -3,7 +3,7 @@ public:
     bool isPrime(int num) {
         if (num == 1)
             return false;
-        for (int i = 2; i < num; i++) {
+        for (int i = 2; i*i <= num; i++) {
             if (num % i == 0)
                 return false;
         }
