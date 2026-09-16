b442a587ce49e51e2535a20d912b3cb60cf27c86
Prakriti <prakriti.kshyp@gmail.com>
2026-09-16 22:01:36 +0530

    Time: 17 ms (53.27%), Space: 26.2 MB (32.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,16 +3,14 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> str;
         unordered_map<string, vector<string>> mp;
-        for (auto it : strs) {
+        for (const string&it : strs) {
             string before = it;
 
-            sort(it.begin(), it.end());
-            mp[it].push_back(before);
+            sort(before.begin(), before.end());
+            mp[before].push_back(it);
         }
 
         for (auto it : mp) {
-            cout<<it.first<<endl;
-
             str.push_back(it.second);
         }
 
