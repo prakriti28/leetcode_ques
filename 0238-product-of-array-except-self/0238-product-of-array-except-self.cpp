5a02397cdd478717d21716d6e7bf14ccf1315c84
Prakriti <prakriti.kshyp@gmail.com>
2026-09-17 00:04:25 +0530

    Time: 3 ms (37.37%), Space: 47 MB (5.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,8 +6,8 @@ public:
         int startPrefix = 1;
         prefix.push_back(1);
         for (int i = 1; i < nums.size(); i++) {
-            prefix.push_back(startPrefix * nums[i - 1]);
             startPrefix = startPrefix * nums[i - 1];
+            prefix.push_back(startPrefix);
         }
         int startSuffix = nums[nums.size() - 1];
         suffix.push_back(1);
@@ -15,11 +15,10 @@ public:
             suffix.push_back(startSuffix);
             startSuffix = startSuffix * nums[j];
         }
-        reverse(suffix.begin(),suffix.end());
-        vector<int>res;
-        for(int k=0;k<suffix.size();k++){
-            res.push_back(suffix[k]*prefix[k]);
-
+        reverse(suffix.begin(), suffix.end());
+        vector<int> res;
+        for (int k = 0; k < suffix.size(); k++) {
+            res.push_back(suffix[k] * prefix[k]);
         }
         return res;
     }
